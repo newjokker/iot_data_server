a44ddaea77e955d564c97bd1822e95b27e486008
jokker <747225581@qq.com>
2025-07-20 00:24:13 +0800

    feat:尝试自动部署的代码

--- auto_amount/auto_amount.sh
+++ auto_amount/auto_amount.sh
@@ -1,32 +1,32 @@
 #!/bin/bash 
 
 # Check if escalator_algo_server is running
-if systemctl is-active --quiet escalator_algo_server.service; then
-    echo "escalator_algo_server.service is running. Stopping it..."
-    sudo systemctl stop escalator_algo_server.service
+if systemctl is-active --quiet iot_data_server.service; then
+    echo "iot_data_server.service is running. Stopping it..."
+    sudo systemctl stop iot_data_server.service
     echo "Service stopped."
 fi
 
 echo "Copy server to /etc/systemd/system"
-sudo cp ./escalator_algo_server.service  /etc/systemd/system/escalator_algo_server.service
+sudo cp ./iot_data_server.service  /etc/systemd/system/iot_data_server.service
 
 # Reload systemd daemon, enable, and start the service
 echo "Reloading systemd daemon..."
 sudo systemctl daemon-reload
 
-echo "Enabling escalator_algo_server.service..."
-sudo systemctl enable escalator_algo_server.service
+echo "Enabling iot_data_server.service..."
+sudo systemctl enable iot_data_server.service
 
-echo "Starting escalator_algo_server.service..."
-sudo systemctl start escalator_algo_server.service
+echo "Starting iot_data_server.service..."
+sudo systemctl start iot_data_server.service
 
 # Wait for 20 seconds
 echo "Wait for start..."
 sleep 10
 
 # Check the status of the service
-echo "Checking the status of escalator_algo_server.service..."
-sudo systemctl status escalator_algo_server.service
+echo "Checking the status of iot_data_server.service..."
+sudo systemctl status iot_data_server.service
 
 # 查看实时日志
 # journalctl -u escalator_algo_server.service -f 
